dbd3630c65edaa5a10fda9bab7249a30b6e0bb46
David Landry <dwlandry@gmail.com>
2019-07-02 00:17:24 -0500

    Added NominalPipeSizes and Piping Navigation Item.

---
+++ LPO_XAF_v2._0.Module/BusinessObjects/Piping/Piping.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LPO_XAF_v2._0.Module.BusinessObjects.Piping
+{
+    public class PipingMetallurgy : BaseObject
+    {
+        
+        public PipingMetallurgy(Session session) : base(session) { }
+        
+    }
+
+    [DefaultClassOptions,CreatableItem(false), NavigationItem("Piping")]
+    public class NominalPipeSize : BaseObject
+    {
+        
+        public NominalPipeSize(Session session) : base(session) { }
+        //public NominalPipeSize( Session session, double nominalSizeInInches, double outerDiameterInInches) : base(session)
+        //{
+        // 
+        //    this.nominalSizeInInches = nominalSizeInInches;
+        //    this.outerDiameterInInches = outerDiameterInInches;
+        //}
+
+        [Persistent("OuterDiameterInInches")]
+        double outerDiameterInInches;
+        [Persistent("NominalSizeInInches")]
+        double nominalSizeInInches;
+
+        [PersistentAlias("nominalSizeInInches")]
+        [ReadOnly(true)]
+        public double NominalSizeInInches
+        {
+            get => nominalSizeInInches;
+            set => SetPropertyValue(nameof(NominalSizeInInches), ref nominalSizeInInches, value);
+        }
+
+        [PersistentAlias("outerDiameterInInches")]
+        [ReadOnly(true)]
+        public double OuterDiameterInInches
+        {
+            get => outerDiameterInInches;
+            set => SetPropertyValue(nameof(OuterDiameterInInches), ref outerDiameterInInches, value);
+        }
+
+        public double OuterSurfaceAreaPerFootInFeetSquared
+        {
+            get
+            {
+                return OuterDiameterInInches / 12 * Math.PI;
+            }
+        }
+    }
+}
--- LPO_XAF_v2._0.Module/DatabaseUpdate/Updater.cs
+++ LPO_XAF_v2._0.Module/DatabaseUpdate/Updater.cs
@@ -12,6 +12,7 @@ using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.Persistent.BaseImpl.PermissionPolicy;
 using LPO_XAF_v2._0.Module.BusinessObjects.Instrument;
+using LPO_XAF_v2._0.Module.BusinessObjects.Piping;
 using System;
 using System.Linq;
 
@@ -36,6 +37,7 @@ namespace LPO_XAF_v2._0.Module.DatabaseUpdate
             CreateDefaultRole();
             SupplyInitialDataForInstrumentMeasurementCategories();
             SupplyInitialDataForInstrumentDeviceCategories();
+            SupplyNominalPipeSizeData();
         }
         public override void UpdateDatabaseBeforeUpdateSchema()
         {
@@ -95,12 +97,67 @@ namespace LPO_XAF_v2._0.Module.DatabaseUpdate
             if (!InstrumentDeviceCategoryExists("Indicator")) ObjectSpace.CreateObject<InstrumentDeviceCategory>().Name = "Indicator";
             if (!InstrumentDeviceCategoryExists("Transducer")) ObjectSpace.CreateObject<InstrumentDeviceCategory>().Name = "Transducer";
             if (!InstrumentDeviceCategoryExists("Signal Conditioner")) ObjectSpace.CreateObject<InstrumentDeviceCategory>().Name = "Signal Conditioner";
+            ObjectSpace.CommitChanges();
         }
         private bool InstrumentDeviceCategoryExists(string categoryName)
         {
             InstrumentDeviceCategory category = ObjectSpace.FindObject<InstrumentDeviceCategory>(
                 CriteriaOperator.Parse("Name=?", categoryName));
             return category != null;
+        }
+
+        private void SupplyNominalPipeSizeData()
+        {
+            if (!NominalPipeSizeExists(0.125)) AddNominalPipeSize(0.125, 0.405);
+            if (!NominalPipeSizeExists(0.25)) AddNominalPipeSize(0.25, 0.54);
+            if (!NominalPipeSizeExists(0.375)) AddNominalPipeSize(0.375, 0.675);
+            if (!NominalPipeSizeExists(0.5)) AddNominalPipeSize(0.5, 0.84);
+            if (!NominalPipeSizeExists(0.75)) AddNominalPipeSize(0.75, 1.05);
+            if (!NominalPipeSizeExists(1)) AddNominalPipeSize(1, 1.315);
+            if (!NominalPipeSizeExists(1.25)) AddNominalPipeSize(1.25, 1.66);
+            if (!NominalPipeSizeExists(1.5)) AddNominalPipeSize(1.5, 1.9);
+            if (!NominalPipeSizeExists(2)) AddNominalPipeSize(2, 2.375);
+            if (!NominalPipeSizeExists(2.5)) AddNominalPipeSize(2.5, 2.875);
+            if (!NominalPipeSizeExists(3)) AddNominalPipeSize(3, 3.5);
+            if (!NominalPipeSizeExists(3.5)) AddNominalPipeSize(3.5, 4);
+            if (!NominalPipeSizeExists(4)) AddNominalPipeSize(4, 4.5);
+            if (!NominalPipeSizeExists(4.5)) AddNominalPipeSize(4.5, 5);
+            if (!NominalPipeSizeExists(5)) AddNominalPipeSize(5, 5.563);
+            if (!NominalPipeSizeExists(6)) AddNominalPipeSize(6, 6.625);
+            if (!NominalPipeSizeExists(7)) AddNominalPipeSize(7, 7.625);
+            if (!NominalPipeSizeExists(8)) AddNominalPipeSize(8, 8.625);
+            if (!NominalPipeSizeExists(9)) AddNominalPipeSize(9, 9.625);
+            if (!NominalPipeSizeExists(10)) AddNominalPipeSize(10, 10.75);
+            if (!NominalPipeSizeExists(11)) AddNominalPipeSize(11, 11.75);
+            if (!NominalPipeSizeExists(12)) AddNominalPipeSize(12, 12.75);
+            if (!NominalPipeSizeExists(14)) AddNominalPipeSize(14, 14);
+            if (!NominalPipeSizeExists(16)) AddNominalPipeSize(16, 16);
+            if (!NominalPipeSizeExists(18)) AddNominalPipeSize(18, 18);
+            if (!NominalPipeSizeExists(20)) AddNominalPipeSize(20, 20);
+            if (!NominalPipeSizeExists(22)) AddNominalPipeSize(22, 22);
+            if (!NominalPipeSizeExists(24)) AddNominalPipeSize(24, 24);
+            if (!NominalPipeSizeExists(26)) AddNominalPipeSize(26, 26);
+            if (!NominalPipeSizeExists(28)) AddNominalPipeSize(28, 28);
+            if (!NominalPipeSizeExists(30)) AddNominalPipeSize(30, 30);
+            if (!NominalPipeSizeExists(32)) AddNominalPipeSize(32, 32);
+            if (!NominalPipeSizeExists(34)) AddNominalPipeSize(34, 34);
+            if (!NominalPipeSizeExists(36)) AddNominalPipeSize(36, 36);
+            if (!NominalPipeSizeExists(42)) AddNominalPipeSize(42, 42);
+            if (!NominalPipeSizeExists(48)) AddNominalPipeSize(48, 48);
+            ObjectSpace.CommitChanges();
+
+        }
+        private bool NominalPipeSizeExists(double nominalPipeSize)
+        {
+            NominalPipeSize size = ObjectSpace.FindObject<NominalPipeSize>(
+                CriteriaOperator.Parse("NominalSizeInInches=?", nominalPipeSize));
+            return size != null;
+        }
+        private void AddNominalPipeSize(double size, double od)
+        {
+            NominalPipeSize pipe = ObjectSpace.CreateObject<NominalPipeSize>();
+            pipe.NominalSizeInInches = size;
+            pipe.OuterDiameterInInches = od;
         }
     }
 }
