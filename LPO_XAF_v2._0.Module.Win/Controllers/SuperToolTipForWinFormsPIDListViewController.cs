e5c5d2c9416b473194372d890bc8297cd627e7e9
David Landry <dwlandry@gmail.com>
2019-07-28 17:25:01 -0500

    More work on the SuperToolTipForWinFormsPIDListViewController.  Finished the code that will correctly the detail view of the various hyperlinks in the ToolTip.

--- LPO_XAF_v2._0.Module.Win/Controllers/SuperToolTipForWinFormsPIDListViewController.cs
+++ LPO_XAF_v2._0.Module.Win/Controllers/SuperToolTipForWinFormsPIDListViewController.cs
@@ -1,6 +1,9 @@
 ﻿using DevExpress.Data.Filtering;
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.SystemModule;
 using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.Persistent.BaseImpl;
 using DevExpress.Utils;
 using DevExpress.XtraGrid;
 using DevExpress.XtraGrid.Views.Base;
@@ -8,8 +11,10 @@ using DevExpress.XtraGrid.Views.Base.ViewInfo;
 using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraGrid.Views.Grid.ViewInfo;
 using LPO_XAF_v2._0.Module.BusinessObjects.Instrument;
+using LPO_XAF_v2._0.Module.BusinessObjects.Piping;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,7 +31,9 @@ namespace LPO_XAF_v2._0.Module.Win.Controllers
         {
             TargetObjectType = typeof(PID);
             TargetViewNesting = Nesting.Any;
+
         }
+
 
         protected override void OnViewControlsCreated()
         {
@@ -51,7 +58,64 @@ namespace LPO_XAF_v2._0.Module.Win.Controllers
 
         private void ToolTipController_HyperlinkClick(object sender, HyperlinkClickEventArgs e)
         {
-            throw new NotImplementedException();
+            string[] split = e.Link.Split(new char[] { ':' });
+            var category = split[0];
+            var item = split[1];
+
+            if (category == HyperlinkCategory.OpenFile.ToString())
+            {
+                PID pid = ObjectSpace.FindObject<PID>(new BinaryOperator("Oid", item));
+                FileData fd = pid.File;
+                var tempFolder = System.IO.Path.GetTempPath();
+                var filename = System.IO.Path.Combine(tempFolder, fd.FileName);
+                System.IO.File.WriteAllBytes(filename, fd.Content);
+                Process.Start(filename);
+            }
+            else if (category == HyperlinkCategory.OpenRecord.ToString())
+            {
+                IObjectSpace space = Application.CreateObjectSpace(typeof(PID));
+                PID pid = space.FindObject<PID>(new BinaryOperator("Oid", item));
+
+                ShowViewParameters svp = new ShowViewParameters();
+                DetailView dv = Application.CreateDetailView(space, pid);
+                svp.CreatedView = dv;
+                Application.ShowViewStrategy.ShowView(svp, new ShowViewSource(null, null));
+
+            }
+            else if (category == HyperlinkCategory.Equipment.ToString())
+            {
+
+            }
+            else if (category == HyperlinkCategory.Instrument.ToString())
+            {
+                IObjectSpace space = Application.CreateObjectSpace(typeof(Instrument));
+                Instrument instrument = space.FindObject<Instrument>(new BinaryOperator("Oid", item));
+
+                ShowViewParameters svp = new ShowViewParameters();
+                DetailView dv = Application.CreateDetailView(space, instrument);
+                svp.CreatedView = dv;
+                Application.ShowViewStrategy.ShowView(svp, new ShowViewSource(null, null));
+            }
+            else if (category == HyperlinkCategory.Line.ToString())
+            {
+                IObjectSpace space = Application.CreateObjectSpace(typeof(Line));
+                Line line = space.FindObject<Line>(new BinaryOperator("Oid", item));
+
+                ShowViewParameters svp = new ShowViewParameters();
+                DetailView dv = Application.CreateDetailView(space, line);
+                svp.CreatedView = dv;
+                Application.ShowViewStrategy.ShowView(svp, new ShowViewSource(null, null));
+            }
+            else if (category == HyperlinkCategory.TiePoint.ToString())
+            {
+                IObjectSpace space = Application.CreateObjectSpace(typeof(PipingTiePoint));
+                PipingTiePoint tiePoint = space.FindObject<PipingTiePoint>(new BinaryOperator("Oid", item));
+
+                ShowViewParameters svp = new ShowViewParameters();
+                DetailView dv = Application.CreateDetailView(space, tiePoint);
+                svp.CreatedView = dv;
+                Application.ShowViewStrategy.ShowView(svp, new ShowViewSource(null, null));
+            }
         }
 
         private void ToolTipController_GetActiveObjectInfo(object sender, ToolTipControllerGetActiveObjectInfoEventArgs e)
@@ -77,9 +141,13 @@ namespace LPO_XAF_v2._0.Module.Win.Controllers
 
                 // PID Header
                 ToolTipTitleItem pidNumber = new ToolTipTitleItem();
-                pidNumber.Text = $"<size=+4>{pid.DrawingNumber}</size> <href=OpenFile><color=145,197,242>Open File</color></href>";
+                pidNumber.Text = $"<size=+4>{pid.DrawingNumber}</size>";
                 superToolTip.Items.Add(pidNumber);
-                
+                ToolTipItem pidLinks = new ToolTipItem();
+                pidLinks.Text = $"<href={HyperlinkCategory.OpenFile}:{pid.Oid}><color=145,197,242>Open File</color></href> <href={HyperlinkCategory.OpenRecord}:{pid.Oid}><color=145,197,242>Open Record</color></href>";
+                superToolTip.Items.Add(pidLinks);
+                superToolTip.Items.Add(new ToolTipSeparatorItem());
+
                 // Instruments
                 var InstrumentCount = pid.Instruments.Count;
                 ToolTipTitleItem titleItemInstruments = new ToolTipTitleItem();
@@ -88,7 +156,7 @@ namespace LPO_XAF_v2._0.Module.Win.Controllers
                 if (InstrumentCount > 0)
                 {
                     ToolTipItem itemInstruments = new ToolTipItem();
-                    itemInstruments.Text = String.Join(", ", pid.Instruments.Select(item => $"<href=Instrument:{item.Oid}><color=145,197,242>{item.TagNumber}</color></href>"));
+                    itemInstruments.Text = String.Join(", ", pid.Instruments.Select(item => $"<href={HyperlinkCategory.Instrument}:{item.Oid}><color=145,197,242>{item.TagNumber}</color></href>"));
                     superToolTip.Items.Add(itemInstruments);
                 }
                 superToolTip.Items.Add(new ToolTipSeparatorItem());
@@ -101,7 +169,7 @@ namespace LPO_XAF_v2._0.Module.Win.Controllers
                 if (LineCount>0)
                 {
                     ToolTipItem itemLines = new ToolTipItem();
-                    itemLines.Text = String.Join(", ", pid.Lines.Select(item => $"<href=Line:{item.Oid}><color=145,197,242>{item.LineNumber}</color></href>"));
+                    itemLines.Text = String.Join(", ", pid.Lines.Select(item => $"<href={HyperlinkCategory.Line}:{item.Oid}><color=145,197,242>{item.LineNumber}</color></href>"));
                     superToolTip.Items.Add(itemLines);
                 }
                 superToolTip.Items.Add(new ToolTipSeparatorItem());
@@ -114,7 +182,7 @@ namespace LPO_XAF_v2._0.Module.Win.Controllers
                 if (TiePointCount>0)
                 {
                     ToolTipItem itemTiePoints = new ToolTipItem();
-                    itemTiePoints.Text = String.Join(", ", pid.TiePoints.Select(item => $"<href=TiePoint:{item.Oid}><color=145,197,242>{item.Number}</color></href>"));
+                    itemTiePoints.Text = String.Join(", ", pid.TiePoints.Select(item => $"<href={HyperlinkCategory.TiePoint}:{item.Oid}><color=145,197,242>{item.Number}</color></href>"));
                     superToolTip.Items.Add(itemTiePoints);
                 }
                 //superToolTip.Items.Add(new ToolTipSeparatorItem());
@@ -132,6 +200,16 @@ namespace LPO_XAF_v2._0.Module.Win.Controllers
             }
             if (info != null)
                 e.Info = info;
+        }
+
+        private enum HyperlinkCategory
+        {
+            OpenFile,
+            OpenRecord,
+            Instrument,
+            Line,
+            TiePoint,
+            Equipment
         }
     }
 }
